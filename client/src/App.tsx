8a715caaa62f786549266e8cf0860715e572ca1c
Madan <madangowda3242@gmail.com>
2025-01-27 15:29:30 +0530

    vite updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
+import './App.css'
 import { Navbar } from "./components/Navbar";
 
 const App = () => {
   return (<>
       <Navbar />
-      App
+      
   </>
   )
 }
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 export const Navbar = () => {
   return (
     <div className="w-full bg-slate-400 p-3 flex items-center justify-between">
-        <h1 className="text-3xl font-bold p-2 text-white">Blogs</h1>
+        <h1 className="text-3xl font-bold p-2 text-white">Simply Blogs</h1>
         <div className="flex space-x-3">
           <button className="bg-white rounded-md px-4 py-2">Login</button>
           <button className="bg-white rounded-md px-4 py-2">Signup</button>
